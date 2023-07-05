7734e2d8d3d906eabb3cb11f8286ea94d5836555
Robson <st.robson.costa@gmail.com>
2023-07-04 23:05:36 -0300

    Implementação do Post no CRUD Back+Front

--- spring/src/main/java/com/robson/controller/CourseController.java
+++ spring/src/main/java/com/robson/controller/CourseController.java
@@ -2,9 +2,13 @@ package com.robson.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.robson.model.Course;
@@ -20,13 +24,22 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
+    /**
+     * @return
+     */
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
     }
 
-   // @PostMapping
-    //public List<Course> save() {
-        
-    //}
+    /**
+     * @param course
+     */
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {        
+        //System.out.println(course.getName());
+        return courseRepository.save(course);
+        //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course)); // neste caso o metodo seria: public ResponseEntity<Course> create(...
+    }
 }
